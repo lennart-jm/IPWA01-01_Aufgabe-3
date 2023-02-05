26dd85f41b217cc3be54fad363a55853347cef61
Lennart <48008968+lennart-jm@users.noreply.github.com>
2023-02-05 18:23:56 +0100

    typos

--- src/routes/FAQs/+page.svelte
+++ src/routes/FAQs/+page.svelte
@@ -5,8 +5,8 @@
 <h1 class="display-5 fw-bold">FAQs</h1>
 <h4>Was macht genau Kleiderspenden Hamburg?</h4>
 <p>
-    Kleiderpenden Hamburg kümmert sich darum, das ihre Kleiderspenden in das
-    gewünschte Zielland an die Bedürftigen Bürger ohne hohe Kostenverteilt
+    Kleiderpenden Hamburg kümmert sich darum, dass ihre Kleiderspenden in das
+    gewünschte Krisengebiet an die bedürftigen Menschen ohne hohe Kosten verteilt
     werden können.
 </p>
 <h4>Welche Spendenländer benötigen weshalb Kleiderspenden?</h4>
@@ -18,6 +18,6 @@
 
 <h4>Wie finanziert sich das Projekt?</h4>
 <p>
-    Das Projekt finanziert sich ausschließlich durch Spenden von der Stadt
-    Hamburg, sowie diverse Firmen, die sich am Projekt beteiligen.
+    Das Projekt finanziert sich ausschließlich durch Spenden der Stadt
+    Hamburg und diverse Firmen, die sich am Projekt beteiligen.
 </p>
